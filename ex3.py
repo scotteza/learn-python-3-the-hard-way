d1627a3d8f6ebb487b491a218ea1c941384ac126
Scott Edwards <scotteza@gmail.com>
2021-09-14 15:19:32 +0100

    Do exercise 3 - maths

---
+++ ex3.py
@@ -0,0 +1,16 @@
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 / 4)
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print(3 + 2 < 5 - 7)
+
+print(5 > -2)
+print(5 >= -2)
+print(5 <= -2)
+
+print (3%2)
+print (4%2)
+
+print (12/7)
+print (22/7)
