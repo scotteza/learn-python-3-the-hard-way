737e5f9eb5ddbd7808404bb461240b857e1328e0
Scott Edwards <scotteza@gmail.com>
2021-09-14 15:06:15 +0100

    Do example 1 - printing to the console

---
+++ ex1.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("How does escaping quotes work in Python? ' """)
+print('It seems that single and double quotes are interchangeable? " ''')
+# print('Let''s comment this line out')
