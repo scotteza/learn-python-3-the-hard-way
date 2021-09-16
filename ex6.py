b970886059c47f6979744e4abfa97057a7b1fdb6
Scott Edwards <scotteza@gmail.com>
2021-09-16 14:50:09 +0100

    Do exercise 6 - more strings

---
+++ ex6.py
@@ -0,0 +1,16 @@
+number_of_dog_breeds = 12
+x = f"There are {number_of_dog_breeds} dog breeds"
+print(x)
+
+print(f"I said '{x}'")
+
+some_number = 42
+format_string = "The meaning of life is {}"
+print(format_string.format(some_number))
+
+a = "abc"
+b = '123'
+
+print(a + ' ' + b)
+
+print(f'ABC 123 {x}')
